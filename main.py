5a91d33041cd8c5c463cdf99a6b4c55f19b7c68f
glow <renancavenaghizuri@gmail.com>
2024-07-01 19:54:07 -0300

    convert data to python dictionary

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import base64
 from requests import post
+import json
 
 load_dotenv()
 
@@ -11,7 +12,7 @@ client_secret = os.getenv("CLIENT_SECRET")
 def get_token():
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
     url = "https://accounts.spotify.com/api/token"
     headers = {
@@ -20,5 +21,11 @@ def get_token():
     }
 
     data = {"grant_type": "client_credentials"}
-    result = post(url, headerrs=headers, data=data)
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+token = get_token()
+print(token)
     
