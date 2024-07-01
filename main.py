2097b1282295b950b3637c366b87f25979c3e248
glow <renancavenaghizuri@gmail.com>
2024-07-01 19:48:33 -0300

    obtain access token

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from dotenv import load_dotenv
 import os
+import base64
+from requests import post
 
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-print(client_id, client_secret)
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headerrs=headers, data=data)
+    
