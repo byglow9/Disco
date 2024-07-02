fa2a214549c43b8d02cb2579cd5cc73c8a63cbea
glow <renancavenaghizuri@gmail.com>
2024-07-02 12:24:41 -0300

    Revert "java fix"
    This reverts commit 4893818102640f1db5add2b66348ba8455907631.

--- static/script.js
+++ static/script.js
@@ -1,31 +1,19 @@
 async function searchArtist() {
     const artistName = document.getElementById('artistName').value;
+    const response = await fetch(`/search?artist_name=${artistName}`);
+    const data = await response.json();
     const songsList = document.getElementById('songsList');
     songsList.innerHTML = '';
-    
-    try {
-        const response = await fetch(`/search?artist_name=${artistName}`);
-        if (!response.ok) {
-            const errorData = await response.json();
-            songsList.innerHTML = `<p>${errorData.error}</p>`;
-            return;
-        }
-        const data = await response.json();
-        
-        if (data.error) {
-            songsList.innerHTML = `<p>${data.error}</p>`;
-        } else {
-            data.songs.forEach((song, index) => {
-                const songItem = document.createElement('li');
-                songItem.textContent = `${index + 1}. ${song}`;
-                songsList.appendChild(songItem);
-            });
-        }
-    } catch (error) {
-        songsList.innerHTML = `<p>Error: ${error.message}</p>`;
+
+    if (data.error) {
+        songsList.innerHTML = `<p>${data.error}</p>`;
+    } else {
+        data.songs.forEach((song, index) => {
+            const songItem = document.createElement('li');
+            songItem.textContent = `${index + 1}. ${song}`;
+            songsList.appendChild(songItem);
+        });
     }
 }
 
 document.getElementById('searchBtn').addEventListener('click', searchArtist);
-
-
